88436236fa7538d955fe580a9e4ea7007dd51823
Francesco Ammirabile <frammirabile@gmail.com>
2019-02-03 01:53:37 +0100

    Remodeling

--- framework/helpers/FormatConverter.php
+++ framework/helpers/FormatConverter.php
@@ -55,6 +55,16 @@ class FormatConverter extends BaseFormatConverter
 
     /**
      * @param string $value
+     * @return string
+     * @throws InvalidConfigException
+     */
+    public static function asYear(string $value): string
+    {
+        return \Yii::$app->formatter->asDate($value, 'Y');
+    }
+
+    /**
+     * @param string $value
      * @return int
      */
     public static function asSeconds(string $value): int
